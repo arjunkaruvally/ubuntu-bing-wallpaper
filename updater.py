9dab889d4cdf7212baedf2636f17e8d976c1f190
arjun23496 <arjun.k018@gmail.com>
2017-01-13 00:29:49 +0530

    Completed Adding Wallpaper

---
+++ updater.py
@@ -0,0 +1,23 @@
+import os
+import urllib2
+import json
+import cStringIO
+import datetime
+
+metadata = urllib2.urlopen("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US").read()
+
+metadata = json.loads(metadata)
+
+image_url = "http://www.bing.com"+metadata[u'images'][0]['url']
+
+file = cStringIO.StringIO(urllib2.urlopen(image_url).read())
+
+now = datetime.datetime.now()
+
+ctr = now.day
+
+fd = open ('pics/file'+str(ctr)+'.jpg', 'w+')
+# populate buf
+fd.write (file.getvalue ())
+
+os.system("/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///home/arjun/open_source/bing-wallpaper-ubuntu/pics/file"+str(ctr)+".jpg")
